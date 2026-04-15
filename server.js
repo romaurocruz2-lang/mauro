b23c04c3ed8214f0ff4b8a19840e880dea26b8ef
romaurocruz2-lang <romaurocruz2@gmail.com>
2026-04-15 17:27:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://romaurocruz2_db_user:IvAlA43mIQQa1sAc@cluster0.0mvwde9.mongodb.net/agendamentos")
-.then(() => console.log("MongoDB conectado"))
-.catch(err => console.log(err));const express = require("express");
+const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,33 +8,47 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// banco simples (memória)
+mongoose.connect("mongodb+srv://romaurocruz2_db_user:IvAlA43mIQQa1sAc@cluster0.0mvwde9.mongodb.net/agendamentos")
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.log(err));
+
+// LOGIN
+const ADMIN_USER = "admin";
+const ADMIN_PASS = "123456";
+
+app.post("/login", (req, res) => {
+  const { user, pass } = req.body;
+
+  if (user === ADMIN_USER && pass === ADMIN_PASS) {
+    res.json({ ok: true });
+  } else {
+    res.json({ ok: false });
+  }
+});
+
+// AGENDAMENTOS
 let agendamentos = [];
 
-// criar agendamento
 app.post("/agendar", (req, res) => {
   const { nome, profissional, servico, data, horario } = req.body;
 
-  const novo = {
+  agendamentos.push({
     id: Date.now(),
     nome,
     profissional,
     servico,
     data,
     horario
-  };
+  });
 
-  agendamentos.push(novo);
-
-  res.json({ ok: true, message: "Agendamento criado com sucesso" });
+  res.json({ ok: true });
 });
 
-// listar agendamentos
 app.get("/agendamentos", (req, res) => {
   res.json(agendamentos);
 });
 
-// painel admin
+// ADMIN
 app.get("/admin", (req, res) => {
   res.json({
     total: agendamentos.length,
@@ -45,6 +56,7 @@ app.get("/admin", (req, res) => {
   });
 });
 
+// START
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
